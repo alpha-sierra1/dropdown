2368eeccb75e6be6583c5d1392fe045fe04f6949
Rishabh Mehta <rishabh.mh1@gmail.com>
2023-08-17 22:49:57 +0530

    sldfkj

--- script.js
+++ script.js
@@ -8,24 +8,38 @@ const dropdownButton = document.getElementById('dropdown');
 const dropdownButton2 = document.getElementById('dropdown2');
 const dropdownButton3 = document.getElementById('dropdown3');
 
-dropdownButton.addEventListener('click', () => {
-  if (firstList.style.display === 'none') {
-    firstList.style.display = 'block';
-  } else {
-    firstList.style.display = 'none';
-  }
-});
-dropdownButton2.addEventListener('click', () => {
-  if (secondList.style.display === 'none') {
-    secondList.style.display = 'block';
-  } else {
-    secondList.style.display = 'none';
-  }
-});
-dropdownButton3.addEventListener('click', () => {
-  if (thirdList.style.display === 'none') {
-    thirdList.style.display = 'block';
-  } else {
-    thirdList.style.display = 'none';
-  }
-});
+function dropdownHandler(el, target) {
+  el.addEventListener('click', () => {
+    if (target.style.display === 'none') {
+      target.style.display = 'block';
+    } else {
+      target.style.display = 'none';
+    }
+  });
+}
+
+dropdownHandler(dropdownButton, firstList);
+dropdownHandler(dropdownButton2, secondList);
+dropdownHandler(dropdownButton3, thirdList);
+
+// dropdownButton.addEventListener('click', () => {
+//   if (firstList.style.display === 'none') {
+//     firstList.style.display = 'block';
+//   } else {
+//     firstList.style.display = 'none';
+//   }
+// });
+// dropdownButton2.addEventListener('click', () => {
+//   if (secondList.style.display === 'none') {
+//     secondList.style.display = 'block';
+//   } else {
+//     secondList.style.display = 'none';
+//   }
+// });
+// dropdownButton3.addEventListener('click', () => {
+//   if (thirdList.style.display === 'none') {
+//     thirdList.style.display = 'block';
+//   } else {
+//     thirdList.style.display = 'none';
+//   }
+// });
